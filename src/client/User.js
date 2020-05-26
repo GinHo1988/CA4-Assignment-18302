42f2a16cdab84e3633bb6b96be6870c4760a922f
GinHo1988 <ginhonchengho88@gmail.com>
2020-05-26 18:51:48 +0000

    update

---
+++ src/client/User.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import the Link component to use for linking prop information
+import { Link } from 'react-router-dom';
+
+// define one single user card component
+class User extends React.Component {
+  render() {
+    return (
+      <div className="column is-2" style={{ padding: "20px" }}>
+        <div className="card" style={{ borderRadius: "20px" }}>
+          <div className="card-image">
+            <figure className="image is-4by3">
+              <img alt="Profile" src={this.props.picture} />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-content">
+                <p className="title is-4 has-text-primary">{this.props.title}</p>
+                <hr/>
+                <label className="label"> Author: <div className="subtitle is-size-6">{this.props.Author}</div></label>
+                <label className="label"> Publish: <p className="subtitle is-size-6">{this.props.Publish}</p></label>
+                <label className="label"> Comment: <p className="subtitle is-size-6">{this.props.Comment}</p></label>
+                {/*delete the prop with requested id from the function invoked in the parent component*/}
+                <button className="button is-danger" type="button" onClick={() => {this.props.handleDelete(this.props.id);}}>
+                  Delete
+                </button>
+                {/*load the EditUser component via React Router and send the id over to the EditUser component*/}
+                <Link to={`/edit-user/${this.props.id}`}>
+                  <button className="button is-primary" type="button">
+                  Edit
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default User;
